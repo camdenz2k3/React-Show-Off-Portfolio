bef80c011924287567129a38573f00abc8022dd3
Snow Cactus <camdenz2k3@gmail.com>
2023-02-16 03:08:13 -0600

    added some saves that were supposed to be in the last commit

--- src/components/project.js
+++ src/components/project.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { removeHyphensAndCapitalize } from '../../utils/helpers';
+import { removeHyphensAndCapitalize } from '../utils/helpers';
 
 function Project({ project }) {
 
